ebafec411aa41b70349b9267ee3059cd2369e766
ausma-tc <t.corbieres@gmail.com>
2023-03-03 23:19:37 +0100

    added cart dropdown, product components et json

--- src/App.js
+++ src/App.js
@@ -2,14 +2,7 @@ import { Routes, Route, Outlet } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.components";
 import Home from "./routes/home/home.component";
 import Authentication from './routes/authentication/authentication.component';
-
-const Shop = () => {
-  return (
-    <div>
-      <h1>I am the shop page</h1>
-    </div>
-  );
-};
+import Shop from './routes/shop/shop.component';
 
 const App = () => {
   return (
